e39b57a1605ef2523e447ce10d53a310509db68d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:39:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/services_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['services_app_description'] = 'Servicedirektivet app bruges til at styre systemet dæmoner og deres kørende tilstand og giver et nyttigt overblik. Du kan også styre hvilke tjenester starter op ved opstart.';
+$lang['services_app_name'] = 'Services';
+$lang['services_boot_status'] = 'Start op';
+$lang['services_description'] = 'servicebeskrivelse';
+$lang['services_service'] = 'Service';
+$lang['services_services'] = 'Services';
