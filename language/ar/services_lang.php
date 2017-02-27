ed221174e5108bd551f0752fd0741835cc3740fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:37:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/services_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['services_app_description'] = 'يتم استخدام التطبيق خدمات للسيطرة على الشياطين النظام والدولة تشغيل ويقدم نظرة مفيدة. ';
+$lang['services_app_name'] = 'خدمات';
+$lang['services_boot_status'] = 'Startup';
+$lang['services_description'] = 'Service Description';
+$lang['services_service'] = 'Service';
+$lang['services_services'] = 'Services';
