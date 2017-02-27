b1f18b58c415d5c2c1a092c418c9ecefdaf03f8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:39:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/services_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['services_app_description'] = 'Njezin je aplikacija se koristi za kontrolu demone sustava i njihovo trčanje stanje i daje koristan pregled. Također možete kontrolirati koje usluge pokreće prilikom podizanja sustava.';
+$lang['services_app_name'] = 'usluge';
+$lang['services_boot_status'] = 'Stavljanje u pogon';
+$lang['services_description'] = 'Service Description';
+$lang['services_service'] = 'Servis';
+$lang['services_services'] = 'usluge';
