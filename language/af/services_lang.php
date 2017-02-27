1f555fa4965c8302cca4f210044e48f5f1110aa6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:37:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/services_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['services_app_description'] = 'Die Dienste inligting is gebruik om die stelsel daemons en hul lopende toestand te beheer en bied &#39;n nuttige oorsig. Jy kan ook bepaal watter dienste begin op die boot tyd.';
+$lang['services_app_name'] = 'dienste';
+$lang['services_boot_status'] = 'Startup';
+$lang['services_description'] = 'diens beskrywing';
+$lang['services_service'] = 'Service';
+$lang['services_services'] = 'dienste';
