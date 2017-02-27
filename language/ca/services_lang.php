165857c3f9acd741c115a991ee42e98c578f7033
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:38:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/services_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['services_app_description'] = 'L&#39;aplicació Serveis s&#39;utilitza per controlar els dimonis del sistema i el seu estat d&#39;execució i proporciona un panorama útil. ';
+$lang['services_app_name'] = 'Serveis';
+$lang['services_boot_status'] = 'd&#39;inici';
+$lang['services_description'] = 'Descripció del servei';
+$lang['services_service'] = 'servei';
+$lang['services_services'] = 'Serveis';
