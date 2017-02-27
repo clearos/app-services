2be7dab53637da0a775db7108d52863044566136
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:39:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/services_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['services_app_description'] = '该服务的应用程序用来控制系统守护进程和他们的运行状态，并提供了一个有用的概述。 您还可以控制哪些服务在系统启动时启动。';
+$lang['services_app_name'] = '服务';
+$lang['services_boot_status'] = '启动';
+$lang['services_description'] = '服务说明';
+$lang['services_service'] = 'Service';
+$lang['services_services'] = '服务';
