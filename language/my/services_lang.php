34de0d33eb723699b4143d3fe7cee9e34908cc62
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 05:38:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/services_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['services_app_description'] = ' ';
+$lang['services_app_name'] = 'Services';
+$lang['services_boot_status'] = 'Startup';
+$lang['services_description'] = 'Service ကိုဖျေါပွခကျြ';
+$lang['services_service'] = 'ဝန်ဆောင်မှု';
+$lang['services_services'] = 'န်ဆောင်မှုများ';
