6215f20d364cc1d0ef524ce4336e4259a481af9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:56:26 +0000

    Add new files and directories and commit changes to local repository

--- language/eu/services_lang.php
+++ language/eu/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Zerbitzuak aplikazioa sistema daemon eta beraien entzierro egoera kontrolatzeko erabiltzen da, eta ikuspegi erabilgarria eskaintzen du. Ere kontrola dezakezu zein zerbitzuak martxan abioan.';
+$lang['services_app_name'] = 'Zerbitzuak';
+$lang['services_boot_status'] = 'Hasi';
+$lang['services_description'] = 'zerbitzu deskribapena';
+$lang['services_service'] = 'zerbitzua';
+$lang['services_services'] = 'Zerbitzuak';
