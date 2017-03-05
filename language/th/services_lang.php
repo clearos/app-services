ec5bce51c0e4fda2ed9b410b1eb73fd08ac0cbca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:07:35 +0000

    Add new files and directories and commit changes to local repository

--- language/th/services_lang.php
+++ language/th/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'แอปพลิเคบริการจะใช้ในการควบคุมระบบภูตและรัฐทำงานของพวกเขาและให้ภาพรวมที่มีประโยชน์ นอกจากนี้คุณยังสามารถควบคุมการให้บริการเริ่มต้นขึ้นในเวลาบูต';
+$lang['services_app_name'] = 'บริการ';
+$lang['services_boot_status'] = 'การเริ่มต้น';
+$lang['services_description'] = 'รายละเอียดการบริการ';
+$lang['services_service'] = 'บริการ';
+$lang['services_services'] = 'บริการ';
