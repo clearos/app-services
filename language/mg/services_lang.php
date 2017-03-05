05d3fc3bf7d2fe7af3da6262541d41040a35a0a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:02:40 +0000

    Add new files and directories and commit changes to local repository

--- language/mg/services_lang.php
+++ language/mg/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'Ny Services fampiharana no ampiasaina mba hifehy ny rafitra nihazakazaka daemons sy ny fanjakana, ary manome ny mahasoa topy maso. Azonao atao ihany koa ny mifehy izay asa manomboka amin&#39;ny baotin&#39;i ny fotoana.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_boot_status'] = 'Fiatombohana';
 $lang['services_description'] = 'Service Description';
 $lang['services_service'] = 'Service';
 $lang['services_services'] = 'Services';
