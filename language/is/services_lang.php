3059005bd46dcffefd7196d426686e56004f5beb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:00:42 +0000

    Add new files and directories and commit changes to local repository

--- language/is/services_lang.php
+++ language/is/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'The Services app er notað til að stjórna kerfinu daemons og gangi ástand þeirra og veitir gagnlegar yfirlit. Þú getur einnig stjórnað hvaða þjónustu byrjar upp við ræsingu.';
+$lang['services_app_name'] = 'Þjónusta';
+$lang['services_boot_status'] = 'gangsetning';
+$lang['services_description'] = 'þjónusta Lýsing';
+$lang['services_service'] = 'þjónusta';
+$lang['services_services'] = 'Þjónusta';
