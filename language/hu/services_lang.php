aebf16d729c227cfa2857bc22c76fee7b5cb0db1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:00:03 +0000

    Add new files and directories and commit changes to local repository

--- language/hu/services_lang.php
+++ language/hu/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'A Services alkalmazás ellenőrzésére használják a rendszert démonok és működési állapotát, és egy hasznos áttekintést. Azt is szabályozhatja, hogy mely szolgáltatások indulnak el a rendszer indításakor.';
+$lang['services_app_name'] = 'Szolgáltatások';
+$lang['services_boot_status'] = 'üzembe helyezés';
+$lang['services_description'] = 'szolgáltatás leírása';
+$lang['services_service'] = 'Szolgáltatás';
+$lang['services_services'] = 'Szolgáltatások';
