87045a2700cd16b9ebdb9e9a3ce78dd2f8b9558d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:58:14 +0000

    Add new files and directories and commit changes to local repository

--- language/eo/services_lang.php
+++ language/eo/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'La Servoj app estas uzata por kontroli la sistemon demonoj kaj ilia kurado stato kaj provizas utilan superrigardon. ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'La Servoj app estas uzata por kontroli la sistemon demonoj kaj ilia kurado stato kaj provizas utilan superrigardon. Vi povas ankaŭ kontroli kion servoj funkciigas ĉe lanĉtempo.';
+$lang['services_app_name'] = 'servoj';
+$lang['services_boot_status'] = 'lanĉo';
+$lang['services_description'] = 'servo Priskribo';
+$lang['services_service'] = 'servo';
+$lang['services_services'] = 'servoj';
