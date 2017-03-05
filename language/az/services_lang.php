fb1bf7b039376ecf34058fec90ffe6a5e7377e6f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:56:17 +0000

    Add new files and directories and commit changes to local repository

--- language/az/services_lang.php
+++ language/az/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'Services app sistem daemons və onların çalışan dövlət nəzarət etmək üçün istifadə və faydalı ümumi təmin edir. Siz həmçinin xidməti boot zamanda başlamaq olan nəzarət edə bilərsiniz.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
+$lang['services_boot_status'] = 'Başlamaq';
+$lang['services_description'] = 'Service təsviri';
+$lang['services_service'] = 'xidmət';
 $lang['services_services'] = 'Services';
