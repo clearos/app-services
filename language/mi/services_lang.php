44dfcbae57edfa619e43e1e06162a0f62e3b5ad2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:02:50 +0000

    Add new files and directories and commit changes to local repository

--- language/mi/services_lang.php
+++ language/mi/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'whakamahia te taupānga Ratonga te ki te whakahaere i te daemons pūnaha me to ratou ahua rere, me te whakarato i tētahi tirohanga whai hua. Ka taea hoki te whakahaere e tīmata ake ratonga i te wā whawhai.';
+$lang['services_app_name'] = 'ratonga';
+$lang['services_boot_status'] = 'whakaoho';
+$lang['services_description'] = 'Whakaahuatanga ratonga';
+$lang['services_service'] = 'ratonga';
+$lang['services_services'] = 'ratonga';
