f64cf1e2a7ab101ec7c62878a9ad2e48ad67f47b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:08:06 +0000

    Add new files and directories and commit changes to local repository

--- language/uk/services_lang.php
+++ language/uk/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Додаток Services використовується для управління системою демонами і їх робочому стані і забезпечує корисний огляд. Ви також можете контролювати, які служби запускаються під час завантаження.';
+$lang['services_app_name'] = 'послуги';
+$lang['services_boot_status'] = 'Стартап';
+$lang['services_description'] = 'опис послуги';
+$lang['services_service'] = 'обслуговування';
+$lang['services_services'] = 'послуги';
