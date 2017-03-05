222a4d1dd478f43af0e96f94310eabb12f11481a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:57:04 +0000

    Add new files and directories and commit changes to local repository

--- language/bg/services_lang.php
+++ language/bg/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Приложението на услугите се използва за контролиране на демоните на системата и тяхното движение държавата и осигурява полезен преглед. ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_app_description'] = 'Приложението на услугите се използва за контролиране на демоните на системата и тяхното движение държавата и осигурява полезен преглед. Можете също така да контролирате кои услуги да започнат по време на зареждане.';
+$lang['services_app_name'] = 'Услуги';
+$lang['services_boot_status'] = 'Започвам';
 $lang['services_description'] = 'Описание на услугата';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_service'] = 'Обслужване';
+$lang['services_services'] = 'Услуги';
