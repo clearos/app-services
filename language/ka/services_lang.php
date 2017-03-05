11c71fb960cc5fb324dcbfb34791495c66067d2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:01:12 +0000

    Add new files and directories and commit changes to local repository

--- language/ka/services_lang.php
+++ language/ka/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'მომსახურება app გამოიყენება კონტროლის სისტემის დემონები და მათი გაშვებული სახელმწიფო და უზრუნველყოფს სასარგებლო მიმოხილვა. თქვენ ასევე შეგიძლიათ აკონტროლოთ რომელიც მომსახურების დავიწყოთ ჩატვირთვისას.';
+$lang['services_app_name'] = 'მომსახურება';
+$lang['services_boot_status'] = 'დაწყება';
+$lang['services_description'] = 'მომსახურების აღწერა';
+$lang['services_service'] = 'სამსახურის';
+$lang['services_services'] = 'მომსახურება';
