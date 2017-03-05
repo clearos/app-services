a6853a2589f96e8315f8ca000637ee4a8e3a4ac5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:06:16 +0000

    Add new files and directories and commit changes to local repository

--- language/st/services_lang.php
+++ language/st/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'The Services app e sebelisetsoa ho laola daemons tsamaiso le boemong ba bona ba ho matha &#39;me e fana ka tjhebokakaretso e metle. U ka boela laola e leng ditshebeletso qala tsoha ka dia boot nako.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
+$lang['services_boot_status'] = 'E qalang';
+$lang['services_description'] = 'Tlhaloso Service';
 $lang['services_service'] = 'Service';
 $lang['services_services'] = 'Services';
