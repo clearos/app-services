7eb1ffedaab4d20c10bd3db27aa3c3febbec6b07
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:08:46 +0000

    Add new files and directories and commit changes to local repository

--- language/cy/services_lang.php
+++ language/cy/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Mae&#39;r app Gwasanaethau yn cael ei ddefnyddio i reoli daemonau&#39;n system ac mae eu cyflwr yn rhedeg ac yn rhoi trosolwg defnyddiol. Gallwch hefyd yn rheoli pa wasanaethau cychwyn amser ymgychwyn.';
+$lang['services_app_name'] = 'gwasanaethau';
+$lang['services_boot_status'] = 'startup';
+$lang['services_description'] = 'Disgrifiad o&#39;r Gwasanaeth';
+$lang['services_service'] = 'Gwasanaeth';
+$lang['services_services'] = 'gwasanaethau';
