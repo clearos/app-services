3b7f3335ce1e79aa362c687c24bbbb6ceea28bbe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:09:07 +0000

    Add new files and directories and commit changes to local repository

--- language/yo/services_lang.php
+++ language/yo/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
+$lang['services_app_description'] = 'Awọn Iṣẹ app wa ni lo lati šakoso awọn eto daemons ati awọn won yen ipinle ati ki o pese a wulo Akopọ. O tun le sakoso eyi ti awọn iṣẹ bẹrẹ soke ni bata akoko.';
+$lang['services_app_name'] = 'awọn iṣẹ';
+$lang['services_boot_status'] = 'Ibẹrẹ';
+$lang['services_description'] = 'Service Apejuwe';
 $lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_services'] = 'awọn iṣẹ';
