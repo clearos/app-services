4daee0022778ba01a049bb474b7bda0584fe877c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:08:56 +0000

    Add new files and directories and commit changes to local repository

--- language/yi/services_lang.php
+++ language/yi/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'די באַדינונגען אַפּ איז געניצט צו קאָנטראָלירן די סיסטעם דאַעמאָנס און זייער פליסנדיק שטאַט און גיט אַ נוצלעך איבערבליק. איר קענען אויך קאָנטראָלירן וואָס באַדינונגען אָנהייבן זיך ביי שטיוול צייַט.';
+$lang['services_app_name'] = 'באַדינונגען';
+$lang['services_boot_status'] = 'הייב אן';
+$lang['services_description'] = 'א דעסקריפּטיאָן';
+$lang['services_service'] = 'דינסט';
+$lang['services_services'] = 'באַדינונגען';
