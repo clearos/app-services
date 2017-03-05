2dcf7bcfc07364bc7be444087396f2fc1f058359
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:04:27 +0000

    Add new files and directories and commit changes to local repository

--- language/pa/services_lang.php
+++ language/pa/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'ਸਰਵਿਸਿਜ਼ ਐਪ ਸਿਸਟਮ ਡੈਮਨ ਅਤੇ ਆਪਣੇ ਚੱਲ ਰਾਜ ਦੇ ਕੰਟਰੋਲ ਕਰਨ ਲਈ ਵਰਤਿਆ ਹੈ ਅਤੇ ਇੱਕ ਲਾਭਦਾਇਕ ਸੰਖੇਪ ਜਾਣਕਾਰੀ ਦਿੰਦਾ ਹੈ ਹੈ. ਤੁਹਾਨੂੰ ਇਹ ਵੀ ਕੰਟਰੋਲ ਕਰ ਸਕਦੇ ਹੋ, ਜੋ ਕਿ ਸੇਵਾ ਬੂਟ ਹੋਣ ਤੇ ਸ਼ੁਰੂ.';
+$lang['services_app_name'] = 'ਸਰਵਿਸਿਜ਼';
+$lang['services_boot_status'] = 'ਸ਼ੁਰੂ ਕਰਣਾ';
+$lang['services_description'] = 'ਸੇਵਾ ਵੇਰਵਾ';
+$lang['services_service'] = 'ਸੇਵਾ';
+$lang['services_services'] = 'ਸਰਵਿਸਿਜ਼';
