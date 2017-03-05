a4dffcafdf4694368f04188e51097d5fc85b44df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:57:14 +0000

    Add new files and directories and commit changes to local repository

--- language/my/services_lang.php
+++ language/my/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_app_description'] = 'အဆိုပါန်ဆောင်မှုများ app အားစနစ် daemons နှင့်၎င်းတို့၏ပြေးပြည်နယ်ကိုထိန်းချုပ်ဖို့အသုံးပြုပြီးအသုံးဝခြုံငုံသုံးသပ်ချက်ကိုထောက်ပံ့ပေးလျက်ရှိသည်။ သင်တို့သည်လည်းဝန်ဆောင်မှုများကို boot time အချိန်မှာကို start up သောထိန်းချုပ်နိုင်ပါတယ်။';
+$lang['services_app_name'] = 'န်ဆောင်မှုများ';
+$lang['services_boot_status'] = 'စတင်တည်ထောင်သည်';
 $lang['services_description'] = 'Service ကိုဖျေါပွခကျြ';
 $lang['services_service'] = 'ဝန်ဆောင်မှု';
 $lang['services_services'] = 'န်ဆောင်မှုများ';
