a865d87df9c161e3edf8b6b867256a7051e7353c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:59:34 +0000

    Add new files and directories and commit changes to local repository

--- language/ht/services_lang.php
+++ language/ht/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Se app a Sèvis itilize kontwole damon yo sistèm ak eta kouri yo epi li bay yon BECA itil. Ou kapab tou kontwole ki sèvis kòmanse moute nan moman bòt.';
+$lang['services_app_name'] = 'sèvis';
+$lang['services_boot_status'] = 'startup';
+$lang['services_description'] = 'sèvis Deskripsyon';
+$lang['services_service'] = 'Lapòs';
+$lang['services_services'] = 'sèvis';
