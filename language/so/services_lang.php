bd335f61911e6901bbe7cf127bd40bf500ce6a3e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:06:07 +0000

    Add new files and directories and commit changes to local repository

--- language/so/services_lang.php
+++ language/so/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
+$lang['services_app_description'] = 'app Adeegyada waxaa loo isticmaalaa si loo xakameeyo daemons nidaamka gobolka iyo orodka iyo bixinaya aragti guud oo ah mid waxtar leh. Waxa kale oo aad xakamayn kartid adeegyada bilowdo waqtiga boot.';
+$lang['services_app_name'] = 'Adeegyada';
+$lang['services_boot_status'] = 'xawaaladaha';
+$lang['services_description'] = 'Adeegga Description';
 $lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_services'] = 'Adeegyada';
