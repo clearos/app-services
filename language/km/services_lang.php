e0db216764b8215c7ea734a4d591a46542931528
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:01:30 +0000

    Add new files and directories and commit changes to local repository

--- language/km/services_lang.php
+++ language/km/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'សេវាកម្មកម្មវិធីត្រូវបានប្រើដើម្បីត្រួតពិនិត្យដេមិនប្រព័ន្ធនិងរដ្ឋដែលកំពុងរត់របស់ពួកគេនិងផ្តល់នូវទិដ្ឋភាពទូទៅមានប្រយោជន៍។ អ្នកអាចត្រួតពិនិត្យការផ្តល់សេវាចាប់ផ្តើមឡើងនៅពេលចាប់ផ្ដើម។';
+$lang['services_app_name'] = 'សេវាកម្ម';
+$lang['services_boot_status'] = 'ចាប់ផ្តើម';
+$lang['services_description'] = 'សេវាសង្ខេប';
+$lang['services_service'] = 'សេវា';
+$lang['services_services'] = 'សេវាកម្ម';
