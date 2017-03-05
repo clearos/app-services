7cb2203c82a77a10082dee9d5f1e5e8bfafed066
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:02:20 +0000

    Add new files and directories and commit changes to local repository

--- language/lt/services_lang.php
+++ language/lt/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Paslaugų programa yra naudojami siekiant kontroliuoti sistemos demonus ir jų važiavimo būklę ir teikia naudingą apžvalgą. Jūs taip pat gali kontroliuoti, kokias paslaugas pradėti įkrovos metu.';
+$lang['services_app_name'] = 'Paslaugos';
+$lang['services_boot_status'] = 'Pradėti';
+$lang['services_description'] = 'Paslaugų aprašymas';
+$lang['services_service'] = 'tarnyba';
+$lang['services_services'] = 'Paslaugos';
