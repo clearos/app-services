74005b91d23cecd85ed61a42b23186e61366eb41
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:59:43 +0000

    Add new files and directories and commit changes to local repository

--- language/ha/services_lang.php
+++ language/ha/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'The Services app da ake amfani da su iko da tsarin daemons da Gudun jihar da kuma samar da wani amfani Siffar. Zaka kuma iya sarrafa abin da ayyuka fara tashi a taya lokaci.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_boot_status'] = 'Allon farawa';
 $lang['services_description'] = 'Service Description';
 $lang['services_service'] = 'Service';
 $lang['services_services'] = 'Services';
