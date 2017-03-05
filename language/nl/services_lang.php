41c79a08c1ac691c9af7a00b0ea6667a330f28c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:04:08 +0000

    Add new files and directories and commit changes to local repository

--- language/nl/services_lang.php
+++ language/nl/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
+$lang['services_app_description'] = 'De Services app wordt gebruikt om het systeem daemons en hun systeem van toezicht op en geeft een nuttig overzicht. U kunt ook bepalen welke diensten starten bij het opstarten.';
+$lang['services_app_name'] = 'diensten';
+$lang['services_boot_status'] = 'Beginnen';
+$lang['services_description'] = 'dienst Omschrijving';
 $lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_services'] = 'diensten';
