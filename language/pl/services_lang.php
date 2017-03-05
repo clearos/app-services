16cddce1237036af2adb73dbe04679b1578846ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:04:47 +0000

    Add new files and directories and commit changes to local repository

--- language/pl/services_lang.php
+++ language/pl/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Aplikacja Usługi służy do kontrolowania demonów systemowych i ich stan uruchomiony i zapewnia użyteczny przegląd. Można również kontrolować, które zaczynają się usługi podczas startu systemu.';
+$lang['services_app_name'] = 'Usługi';
+$lang['services_boot_status'] = 'Uruchomienie';
+$lang['services_description'] = 'opis usługi';
+$lang['services_service'] = 'Usługa';
+$lang['services_services'] = 'Usługi';
