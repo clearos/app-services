0351e664cad7eb64de7a1ddfa7fa6f087c6e9b4a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:56:46 +0000

    Add new files and directories and commit changes to local repository

--- language/bn/services_lang.php
+++ language/bn/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'সার্ভিস অ্যাপ্লিকেশন সিস্টেম ডেমন ও তাদের পদলেহী রাষ্ট্র নিয়ন্ত্রণ করতেন এবং একটি দরকারী ওভারভিউ উপলব্ধ করা হয়. এছাড়াও আপনি নিয়ন্ত্রণ করতে পারেন যা সেবা বুট করার সময় আরম্ভ.';
+$lang['services_app_name'] = 'সার্ভিস';
+$lang['services_boot_status'] = 'প্রারম্ভ';
+$lang['services_description'] = 'সেবা বর্ণনা';
+$lang['services_service'] = 'সেবা';
+$lang['services_services'] = 'সার্ভিস';
