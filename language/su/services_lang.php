458e5fd661d583cc30eee220e3ac117d25f95b9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:06:35 +0000

    Add new files and directories and commit changes to local repository

--- language/su/services_lang.php
+++ language/su/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'Nu Services app dipaké pikeun ngadalikeun daemons sistim jeung kaayaan jalan maranéhanana jeung nyadiakeun Tinjauan dipake. Anjeun oge bisa ngadalikeun ladenan nu ngamimitian nepi dina waktu boot.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_boot_status'] = 'Ngamimitian';
 $lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
+$lang['services_service'] = 'palayanan';
 $lang['services_services'] = 'Services';
