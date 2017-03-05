c7e8852c389cad388cdf2625884cf033fe7e8b23
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:02:01 +0000

    Add new files and directories and commit changes to local repository

--- language/la/services_lang.php
+++ language/la/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'Services In app moderari solet cursu rei ratio praebet utile Overview daemonum. Te potest etiam, quod Booz usque ad tempus ministerium committitur.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_boot_status'] = 'Start up';
 $lang['services_description'] = 'Service Description';
 $lang['services_service'] = 'Service';
 $lang['services_services'] = 'Services';
