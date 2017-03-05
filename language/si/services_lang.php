15a2d8eeaabc91609adb39e4a5306c5cbfe4def4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:05:37 +0000

    Add new files and directories and commit changes to local repository

--- language/si/services_lang.php
+++ language/si/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'සේවා යෙදුම පද්ධතිය daemons සහ ඔවුන්ගේ ධාවන රාජ්ය පාලනය කිරීම සඳහා භාවිතා සහ ප්රයෝජනවත් දළ විශ්ලේෂණයක් කර ඇත. ඔබ ද සේවා ඇරඹුම් අවස්ථාවේදී ආරම්භ කරන පාලනය කළ හැකිය.';
+$lang['services_app_name'] = 'සේවා';
+$lang['services_boot_status'] = 'පණ ගන්වන්න';
+$lang['services_description'] = 'සේවා විස්තරය';
+$lang['services_service'] = 'සේවය';
+$lang['services_services'] = 'සේවා';
