00eb813cf84861f3613abd50e289d4b08cd11036
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:05:08 +0000

    Add new files and directories and commit changes to local repository

--- language/ro/services_lang.php
+++ language/ro/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'App Services este folosit pentru a controla daemon sistem și starea lor de funcționare și oferă o privire de ansamblu utilă. De asemenea, puteți controla care serviciile de pornire în momentul pornirii.';
+$lang['services_app_name'] = 'Servicii';
+$lang['services_boot_status'] = 'Lansare';
+$lang['services_description'] = 'descrierea serviciului';
+$lang['services_service'] = 'Serviciu';
+$lang['services_services'] = 'Servicii';
