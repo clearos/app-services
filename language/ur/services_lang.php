4414e6b274357bb1506d32edadd00d1cb3117867
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:08:16 +0000

    Add new files and directories and commit changes to local repository

--- language/ur/services_lang.php
+++ language/ur/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'سروسز ایپ کو سسٹم شیطان اور ان کے چلانے ریاست کو کنٹرول کرنے کے استعمال کیا اور ایک مفید جائزہ فراہم کی جاتی ہے. تم نے بھی خدمات بوٹ وقت شروع ہے جس کو کنٹرول کر سکتے.';
+$lang['services_app_name'] = 'سروسز';
+$lang['services_boot_status'] = 'شروع';
+$lang['services_description'] = 'سروس کی تفصیل';
+$lang['services_service'] = 'سروس';
+$lang['services_services'] = 'سروسز';
