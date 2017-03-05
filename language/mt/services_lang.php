08fb0eeb8b834d0e1661cfb0ab8d5e03d1690ae7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:03:48 +0000

    Add new files and directories and commit changes to local repository

--- language/mt/services_lang.php
+++ language/mt/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Il-app Servizzi huwa użat biex jikkontrolla l-daemons tas-sistema u l-istat tmexxija tagħhom u tipprovdi ħarsa ġenerali siewja. Tista &#39;wkoll jikkontrolla liema servizzi jibdew fil-ħin boot.';
+$lang['services_app_name'] = 'Servizzi';
+$lang['services_boot_status'] = 'Ibda';
+$lang['services_description'] = 'servizz Deskrizzjoni';
+$lang['services_service'] = 'servizz';
+$lang['services_services'] = 'Servizzi';
