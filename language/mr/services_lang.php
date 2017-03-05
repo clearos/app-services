1210e6cd6b1e5a6fe03a63fecfadf9e658830fe5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:03:28 +0000

    Add new files and directories and commit changes to local repository

--- language/mr/services_lang.php
+++ language/mr/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'सेवा अनुप्रयोग प्रणाली डीमन आणि त्यांच्या चालू राज्य नियंत्रित करण्यासाठी वापरले आणि उपयोगी अवलोकन पुरवते आहे. आपण सेवा बूट वेळी सुरू नियंत्रित करू शकता.';
+$lang['services_app_name'] = 'सेवा';
+$lang['services_boot_status'] = 'स्टार्टअप';
+$lang['services_description'] = 'सेवा वर्णन';
+$lang['services_service'] = 'सेवा';
+$lang['services_services'] = 'सेवा';
