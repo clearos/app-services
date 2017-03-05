1bcf44dfb4dac5693460c1dab23c3dfcfaf6ef45
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:56:55 +0000

    Add new files and directories and commit changes to local repository

--- language/bs/services_lang.php
+++ language/bs/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
+$lang['services_app_description'] = 'Usluge aplikacija se koristi za kontrolu sistema demone i njihov rad državnih i daje koristan pregled. Također se možete kontrolirati koje usluge pokreće u vrijeme pokretanja.';
+$lang['services_app_name'] = 'usluge';
 $lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_description'] = 'Opis usluge';
+$lang['services_service'] = 'usluga';
+$lang['services_services'] = 'usluge';
