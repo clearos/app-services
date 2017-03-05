73567db7257be0da1f30e2fc0446d4157fc1150f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:05:27 +0000

    Add new files and directories and commit changes to local repository

--- language/sr/services_lang.php
+++ language/sr/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Службе апликација се користи за контролу система демоне и њихове оперативне стање и даје користан преглед. Такође можете да контролишете које услуге старт уп у време подизања система.';
+$lang['services_app_name'] = 'Услуге';
+$lang['services_boot_status'] = 'Покренути';
+$lang['services_description'] = 'Опис услуге';
+$lang['services_service'] = 'сервис';
+$lang['services_services'] = 'Услуге';
