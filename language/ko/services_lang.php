18325b3344a93e205d04d3965f66b732bf056ebc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:01:50 +0000

    Add new files and directories and commit changes to local repository

--- language/ko/services_lang.php
+++ language/ko/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = '서비스 응용 프로그램은 시스템 데몬 및 실행 상태를 제어하는 ​​데 사용하고 유용한 개요를 제공합니다. 또한 서비스가 부팅시에 시작되는 제어 할 수 있습니다.';
+$lang['services_app_name'] = '서비스';
+$lang['services_boot_status'] = '시작';
+$lang['services_description'] = '서비스 설명';
+$lang['services_service'] = '서비스';
+$lang['services_services'] = '서비스';
