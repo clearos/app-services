4115a6654ae3e5875e4c4c0ffca15e2994cd3b2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:02:10 +0000

    Add new files and directories and commit changes to local repository

--- language/lo/services_lang.php
+++ language/lo/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'ການ app ບໍລິການໄດ້ຖືກນໍາໃຊ້ໃນການຄວບຄຸມ daemons ລະບົບແລະລັດເຮັດວຽກຂອງເຂົາເຈົ້າແລະການສະຫນອງສະພາບລວມເປັນປະໂຫຍດ. ນອກນັ້ນທ່ານຍັງສາມາດຄວບຄຸມທີ່ການບໍລິການເລີ່ມຕົ້ນຂຶ້ນໃນເວລາ boot.';
+$lang['services_app_name'] = 'ການບໍລິການ';
+$lang['services_boot_status'] = 'ເລີ່ມ​ຕົ້ນ';
+$lang['services_description'] = 'ລາຍລະອຽດເກມການບໍລິການ';
+$lang['services_service'] = 'ການບໍລິການ';
+$lang['services_services'] = 'ການບໍລິການ';
