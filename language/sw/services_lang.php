c6b78dc1c0c3e37b7fba092340ab8b5d643ae376
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:06:45 +0000

    Add new files and directories and commit changes to local repository

--- language/sw/services_lang.php
+++ language/sw/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'Services programu hutumika kudhibiti daemons mfumo na hali zao mbio na inatoa maelezo muhimu. Unaweza pia kudhibiti ambayo huduma kuanza hadi saa Boot wakati.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
+$lang['services_boot_status'] = 'Anzisha';
+$lang['services_description'] = 'huduma Maelezo ya';
+$lang['services_service'] = 'huduma';
 $lang['services_services'] = 'Services';
