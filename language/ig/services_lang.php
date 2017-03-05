5705d7caeada3ead0eb234bbb55f51390fecfe22
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:00:32 +0000

    Add new files and directories and commit changes to local repository

--- language/ig/services_lang.php
+++ language/ig/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'The Services ngwa na-eji aka ịchịkwa usoro daemons na ha na-agba ọsọ ala ma na-enye a bara uru nnyocha. I nwekwara ike ịchịkwa nke ọrụ na-amalite ruo mgbe buut oge.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_boot_status'] = 'Malite';
 $lang['services_description'] = 'Service Description';
 $lang['services_service'] = 'Service';
 $lang['services_services'] = 'Services';
