431ffda2294c17b849e16d24d9b9bb23aa442e4a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:01:02 +0000

    Add new files and directories and commit changes to local repository

--- language/ja/services_lang.php
+++ language/ja/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'サービスアプリケーションは、システムデーモンとその実行状態を制御するために使用される有用な概要を提供します。 また、サービスがブート時に起動するかを制御することができます。';
+$lang['services_app_name'] = 'サービス';
+$lang['services_boot_status'] = '起動';
+$lang['services_description'] = 'サービス記述';
+$lang['services_service'] = 'サービス';
+$lang['services_services'] = 'サービス';
