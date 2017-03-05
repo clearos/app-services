3e8846ad40ae841e83c77decf6e6bbee3091c9e3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:07:55 +0000

    Add new files and directories and commit changes to local repository

--- language/tr/services_lang.php
+++ language/tr/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Hizmetler Uygulamanın sistem cinleri ve çalışma durumunu kontrol etmek için kullanılan ve yararlı bir bakış sağlar. Ayrıca hizmet boot zamanında başlatmak kontrol edebilirsiniz.';
+$lang['services_app_name'] = 'Hizmetler';
+$lang['services_boot_status'] = 'Başlamak';
+$lang['services_description'] = 'Servis açıklaması';
+$lang['services_service'] = 'Hizmet';
+$lang['services_services'] = 'Hizmetler';
