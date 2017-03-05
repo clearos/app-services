3c207007892afde8e4ae3a0300c985533c425e4f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:03:09 +0000

    Add new files and directories and commit changes to local repository

--- language/ml/services_lang.php
+++ language/ml/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'സേവനങ്ങൾ അപ്ലിക്കേഷൻ സിസ്റ്റം ഡെമണുകൾ അവരുടെ ഓട്ടം സംസ്ഥാന നിയന്ത്രിക്കാൻ ഉപയോഗിക്കുന്ന ഒരു ഉപയോഗപ്രദമായ അവലോകനം നൽകുന്നു ആണ്. നിങ്ങൾക്ക് സേവനങ്ങൾ ബൂട്ട് സമയത്ത് ആരംഭിക്കുമ്പോൾ നിയന്ത്രിക്കാൻ കഴിയും.';
+$lang['services_app_name'] = 'സേവനങ്ങള്';
+$lang['services_boot_status'] = 'സ്റ്റാർട്ടപ്പ്';
+$lang['services_description'] = 'സേവന വിവരണം';
+$lang['services_service'] = 'സേവനം';
+$lang['services_services'] = 'സേവനങ്ങള്';
