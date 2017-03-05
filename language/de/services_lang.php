d5b6e656e6387b699051050346bc761d0750d1d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:59:05 +0000

    Add new files and directories and commit changes to local repository

--- language/de/services_lang.php
+++ language/de/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Die Dienste App wird verwendet, um die System-Daemons und ihre Laufzustand zu steuern und bietet einen nützlichen Überblick. Sie können auch festlegen, welche Dienste beim Booten starten.';
+$lang['services_app_name'] = 'Dienstleistungen';
+$lang['services_boot_status'] = 'Anfang';
+$lang['services_description'] = 'Leistungsbeschreibung';
+$lang['services_service'] = 'Bedienung';
+$lang['services_services'] = 'Dienstleistungen';
