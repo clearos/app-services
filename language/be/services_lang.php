884d2e1a976ffa848c9f36e73b7366a11e4ef464
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:56:36 +0000

    Add new files and directories and commit changes to local repository

--- language/be/services_lang.php
+++ language/be/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Дадатак Services выкарыстоўваецца для кіравання сістэмай дэманамі і іх працоўным стане і забяспечвае карысны агляд. Вы таксама можаце кантраляваць, якія службы запускаюцца падчас загрузкі.';
+$lang['services_app_name'] = 'паслугі';
+$lang['services_boot_status'] = 'Запуск';
+$lang['services_description'] = 'апісанне паслугі';
+$lang['services_service'] = 'абслугоўванне';
+$lang['services_services'] = 'паслугі';
