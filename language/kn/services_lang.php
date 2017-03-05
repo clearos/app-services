006070f70cf0141868a717485e534ee155646714
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:01:40 +0000

    Add new files and directories and commit changes to local repository

--- language/kn/services_lang.php
+++ language/kn/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'ಸೇವೆಗಳು ಅಪ್ಲಿಕೇಶನ್ ಸಿಸ್ಟಮ್ ಡೀಮನ್ ಹಾಗು ಅವರ ಓಟದ ರಾಜ್ಯದ ನಿಯಂತ್ರಿಸಲು ಬಳಸಲಾಗುತ್ತದೆ ಮತ್ತು ಉಪಯುಕ್ತ ಅವಲೋಕನ ಒದಗಿಸುತ್ತದೆ. ನೀವು ಇದು ಸೇವೆಗಳು ಬೂಟ್ ಸಮಯದಲ್ಲಿ ಪ್ರಾರಂಭಿಸಲು ನಿಯಂತ್ರಿಸಬಹುದು.';
+$lang['services_app_name'] = 'ಸೇವೆಗಳು';
+$lang['services_boot_status'] = 'ಆರಂಭಿಕ';
+$lang['services_description'] = 'ಸೇವೆ ವಿವರಣೆ';
+$lang['services_service'] = 'ಸೇವೆ';
+$lang['services_services'] = 'ಸೇವೆಗಳು';
