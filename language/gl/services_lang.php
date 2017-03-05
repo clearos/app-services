1843278084410f1b42481b4f003adef6576770dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:58:54 +0000

    Add new files and directories and commit changes to local repository

--- language/gl/services_lang.php
+++ language/gl/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'A aplicación Servizos úsase para controlar os daemons do sistema e do seu estado de funcionamento e ofrece unha visión útil. Tamén pode controlar cales servizos iniciar no momento da inicialización.';
+$lang['services_app_name'] = 'servizos';
+$lang['services_boot_status'] = 'Posta en marcha';
+$lang['services_description'] = 'Descrición do servizo';
+$lang['services_service'] = 'servizo';
+$lang['services_services'] = 'servizos';
