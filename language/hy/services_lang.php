66f8c5683a334ec46da733acf0b51ffe69449a4b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:56:07 +0000

    Add new files and directories and commit changes to local repository

--- language/hy/services_lang.php
+++ language/hy/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'The Ծառայություններ ծրագիրը օգտագործվում է վերահսկել համակարգի daemons եւ դրանց վարման պետություն եւ ապահովում է օգտակար ակնարկ: Դուք նաեւ կարող եք վերահսկել, որը ծառայություններ է սկսնակ է boot ժամանակ.';
+$lang['services_app_name'] = 'Ծառայություններ';
+$lang['services_boot_status'] = 'Նորաստեղծ';
+$lang['services_description'] = 'Service նկարագրություն';
+$lang['services_service'] = 'ծառայություն';
+$lang['services_services'] = 'Ծառայություններ';
