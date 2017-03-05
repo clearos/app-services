b4a318e1fc35c461343e328dd8942086a6b55152
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:05:46 +0000

    Add new files and directories and commit changes to local repository

--- language/sk/services_lang.php
+++ language/sk/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Aplikácia Services sa používa na ovládanie systému démonmi a ich zapnutého stavu a poskytuje užitočný prehľad. Môžete tiež určiť, ktoré služby spustenie pri štarte systému.';
+$lang['services_app_name'] = 'služby';
+$lang['services_boot_status'] = 'Začiatok';
+$lang['services_description'] = 'Popis služby';
+$lang['services_service'] = 'služba';
+$lang['services_services'] = 'služby';
