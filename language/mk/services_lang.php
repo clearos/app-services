97a1c4cbe8d0259238bed260e101a38cba061fcd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:02:59 +0000

    Add new files and directories and commit changes to local repository

--- language/mk/services_lang.php
+++ language/mk/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'стан на услуги се користи за контрола на демоните на системот и нивните водење држава и обезбедува корисни преглед. Исто така можете да контролирате кои услуги стартувате од времето на подигање.';
+$lang['services_app_name'] = 'услуги';
+$lang['services_boot_status'] = 'Почни';
+$lang['services_description'] = 'Опис на услугата';
+$lang['services_service'] = 'Сервис';
+$lang['services_services'] = 'услуги';
