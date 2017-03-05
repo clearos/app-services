015cac5b0f5bb51e70e70f36e02da9afc8fb62b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:04:37 +0000

    Add new files and directories and commit changes to local repository

--- language/fa/services_lang.php
+++ language/fa/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'برنامه خدمات استفاده می شود برای کنترل ارواح سیستم و دولت در حال اجرا و یک دید کلی مفید است. شما همچنین می توانید کنترل که خدمات راه اندازی در زمان بوت.';
+$lang['services_app_name'] = 'خدمات';
+$lang['services_boot_status'] = 'شروع';
+$lang['services_description'] = 'شرح خدمات';
+$lang['services_service'] = 'سرویس';
+$lang['services_services'] = 'خدمات';
