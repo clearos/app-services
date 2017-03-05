52aaab96235a03ec594f70aa052e8200437ddf8a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:59:24 +0000

    Add new files and directories and commit changes to local repository

--- language/gu/services_lang.php
+++ language/gu/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'સેવાઓ એપ્લિકેશન સિસ્ટમ ડિમન અને તેમના ચાલી રાજ્ય નિયંત્રિત કરવા માટે વપરાય છે અને ઉપયોગી ઝાંખી પૂરી પાડે છે. તમે પણ નિયંત્રિત કરી શકો છો કે જે સેવાઓ બુટ સમયે શરૂ કરો.';
+$lang['services_app_name'] = 'સેવાઓ';
+$lang['services_boot_status'] = 'શરુઆત';
+$lang['services_description'] = 'સેવા વર્ણન';
+$lang['services_service'] = 'સેવા';
+$lang['services_services'] = 'સેવાઓ';
