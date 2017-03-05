3e66311d0d8e68eb3fbe8f8e148b071f9f9d18eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:08:26 +0000

    Add new files and directories and commit changes to local repository

--- language/uz/services_lang.php
+++ language/uz/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Xizmatlar Ilova tizim Daimons va ularning yugurish holatini nazorat qilish uchun foydalaniladigan va foydali umumiy beradi. Bundan tashqari, xizmatlar boot vaqtda boshlash, qaysi nazorat qilish mumkin.';
+$lang['services_app_name'] = 'xizmatlar';
+$lang['services_boot_status'] = 'Ish boshlash';
+$lang['services_description'] = 'xizmat Tavsif';
+$lang['services_service'] = 'xizmat';
+$lang['services_services'] = 'xizmatlar';
