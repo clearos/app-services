2e9e1ba415efaa7cfe1285d24330fcd0bca3c981
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:08:36 +0000

    Add new files and directories and commit changes to local repository

--- language/vi/services_lang.php
+++ language/vi/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
+$lang['services_app_description'] = 'Các ứng dụng của dịch vụ được sử dụng để kiểm soát trình nền hệ thống và trạng thái hoạt động của họ và cung cấp một cái nhìn tổng quan hữu ích. Bạn cũng có thể kiểm soát các dịch vụ khởi động lúc khởi động.';
+$lang['services_app_name'] = 'Dịch vụ';
 $lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_description'] = 'Dịch vụ Mô tả';
+$lang['services_service'] = 'Dịch vụ';
+$lang['services_services'] = 'Dịch vụ';
