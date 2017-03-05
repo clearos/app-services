f1343119299cc85a95b4d54d482eedc9ce9df4d0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:07:15 +0000

    Add new files and directories and commit changes to local repository

--- language/te/services_lang.php
+++ language/te/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'సర్వీసులు అనువర్తనం సిస్టమ్ డెమోన్స్ మరియు వారి రన్నింగ్ రాష్ట్ర నియంత్రించడానికి మరియు ఉపయోగించవచ్చు ఒక ఉపయోగకరమైన అవలోకనాన్ని అందిస్తుంది. మీరు కూడా సేవలు బూట్ సమయంలో ప్రారంభించిన నియంత్రించవచ్చు.';
+$lang['services_app_name'] = 'సేవలు';
+$lang['services_boot_status'] = 'మొదలుపెట్టు';
+$lang['services_description'] = 'సర్వీస్ వివరణ';
+$lang['services_service'] = 'సర్వీస్';
+$lang['services_services'] = 'సేవలు';
