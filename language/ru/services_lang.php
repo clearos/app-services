0535c8927d9991c7c60941c6584f8a8bae6ef7ff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:05:18 +0000

    Add new files and directories and commit changes to local repository

--- language/ru/services_lang.php
+++ language/ru/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Приложение Services используется для управления системой демонами и их рабочем состоянии и обеспечивает полезный обзор. Вы также можете контролировать, какие службы запускаются во время загрузки.';
+$lang['services_app_name'] = 'Сервисы';
+$lang['services_boot_status'] = 'Запускать';
+$lang['services_description'] = 'Описание услуг';
+$lang['services_service'] = 'обслуживание';
+$lang['services_services'] = 'Сервисы';
