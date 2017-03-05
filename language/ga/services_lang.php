2c5995a4d46808bfbf893a1a62f1e8a6aec0079a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:00:23 +0000

    Add new files and directories and commit changes to local repository

--- language/ga/services_lang.php
+++ language/ga/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
+$lang['services_app_description'] = 'Is é an app Seirbhísí a úsáidtear chun rialú a dhéanamh ar na ndeamhan chórais agus a staid reatha agus cuireann sé forbhreathnú úsáideach. Is féidir leat a rialú chomh maith a thosú seirbhísí suas ag am tosaithe.';
+$lang['services_app_name'] = 'seirbhísí';
 $lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_description'] = 'seirbhís Tuairisc';
+$lang['services_service'] = 'seirbhís';
+$lang['services_services'] = 'seirbhísí';
