6d8a03f88447a08b0cbeace890a26d0645bd9bcb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:59:53 +0000

    Add new files and directories and commit changes to local repository

--- language/hi/services_lang.php
+++ language/hi/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'सेवा एप्लिकेशन को सिस्टम डेमॉन और उनके चल रहे राज्य को नियंत्रित करने के लिए इस्तेमाल किया और एक उपयोगी अवलोकन प्रदान करता है। आप यह भी नियंत्रित कर सकते हैं जो सेवाओं बूट समय पर शुरू करते हैं।';
+$lang['services_app_name'] = 'सेवा';
+$lang['services_boot_status'] = 'चालू होना';
+$lang['services_description'] = 'सेवा विवरण';
+$lang['services_service'] = 'सर्विस';
+$lang['services_services'] = 'सेवा';
