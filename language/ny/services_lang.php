df7616415388ac3cdce1fd945679f7621acb8b74
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:57:34 +0000

    Add new files and directories and commit changes to local repository

--- language/ny/services_lang.php
+++ language/ny/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = ' Mukhoza kulamulira amene oyang&#39;anira kuyamba pa jombo nthawi.';
+$lang['services_app_description'] = 'The Services app ntchito kulamulira daemons dongosolo ndi boma kwawo ndipo amapereka mwachidule zothandiza. Mukhoza kulamulira amene oyang&#39;anira kuyamba pa jombo nthawi.';
 $lang['services_app_name'] = 'Services';
 $lang['services_boot_status'] = 'Yambitsani';
 $lang['services_description'] = 'Kufotokozera Service';
