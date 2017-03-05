3abcaa6e9ec8b9b42aa53a035158521b766aeaeb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:07:45 +0000

    Add new files and directories and commit changes to local repository

--- language/tl/services_lang.php
+++ language/tl/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
+$lang['services_app_description'] = 'Ang Mga Serbisyo app ay ginagamit upang makontrol ang sistema daemons at ang kanilang pagtakbo ng estado at nagbibigay ng isang kapaki-pakinabang na pangkalahatang-ideya. Maaari mo ring kontrolin kung aling mga serbisyo simulan up sa boot oras.';
 $lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
+$lang['services_boot_status'] = 'Magsimula';
+$lang['services_description'] = 'Serbisyo Paglalarawan';
+$lang['services_service'] = 'serbisyo';
 $lang['services_services'] = 'Services';
