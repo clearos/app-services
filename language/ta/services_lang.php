24cf4518bfe26b8988e7bee36c5e0c3840675d72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:07:05 +0000

    Add new files and directories and commit changes to local repository

--- language/ta/services_lang.php
+++ language/ta/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'சேவைகள் பயன்பாட்டை அமைப்பு டேமன்கள், தங்கள் இயங்கும் மாநில கட்டுப்படுத்த மற்றும் பயன்படுத்தலாம் ஒரு பயனுள்ள கண்ணோட்டத்தை வழங்குகிறது. நீங்கள் சேவைகளை துவக்க நேரத்தில் தொடங்கும் கட்டுப்படுத்த முடியும்.';
+$lang['services_app_name'] = 'சேவைகள்';
+$lang['services_boot_status'] = 'தொடக்க';
+$lang['services_description'] = 'சேவை விளக்கம்';
+$lang['services_service'] = 'சேவை';
+$lang['services_services'] = 'சேவைகள்';
