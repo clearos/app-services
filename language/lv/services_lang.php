22b2f9f303ee1a02bda48ff759f12d84804ae3fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:02:30 +0000

    Add new files and directories and commit changes to local repository

--- language/lv/services_lang.php
+++ language/lv/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Pakalpojumu app izmanto, lai kontrolētu sistēmas dēmonu un to braukšanas stāvokli un sniedz noderīgu pārskatu. Jūs varat arī kontrolēt, kuras pakalpojumi uzsākšanas sāknēšanas laikā.';
+$lang['services_app_name'] = 'pakalpojumi';
+$lang['services_boot_status'] = 'Uzsākt';
+$lang['services_description'] = 'Pakalpojumu apraksts';
+$lang['services_service'] = 'apkalpošana';
+$lang['services_services'] = 'pakalpojumi';
