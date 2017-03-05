8c3caddef226b59548abfbb51f9528a006886fd2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:06:55 +0000

    Add new files and directories and commit changes to local repository

--- language/sv/services_lang.php
+++ language/sv/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
+$lang['services_app_description'] = 'Tjänstedirektivet app används för att styra system demoner och deras löpande tillstånd och ger en användbar översikt. Du kan också styra vilka tjänster startar vid uppstart.';
+$lang['services_app_name'] = 'tjänster';
+$lang['services_boot_status'] = 'Börja';
+$lang['services_description'] = 'Service beskrivning';
 $lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_services'] = 'tjänster';
