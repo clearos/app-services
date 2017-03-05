ea17548ccbe16ba5a460e5d48d6b745571f50623
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:07:25 +0000

    Add new files and directories and commit changes to local repository

--- language/tg/services_lang.php
+++ language/tg/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Дар барнома хизматрасонӣ истифода бурда мешавад барои назорат daemons системаи ва давлат давида ва шарҳи муфид таъмин менамояд. Шумо инчунин метавонед назорат кунед, ки хизматрасониҳои оғоз то вақти пурборкунӣ.';
+$lang['services_app_name'] = 'Хизматрасониҳо';
+$lang['services_boot_status'] = 'Навкор';
+$lang['services_description'] = 'Хадамоти Тавсифи';
+$lang['services_service'] = 'хизматрасонӣ';
+$lang['services_services'] = 'Хизматрасониҳо';
