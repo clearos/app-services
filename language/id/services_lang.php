8d4e9c9fa8e01a8d886237d1965a099809bc9e2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:00:13 +0000

    Add new files and directories and commit changes to local repository

--- language/id/services_lang.php
+++ language/id/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = ' ';
-$lang['services_app_name'] = 'Services';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_app_description'] = 'Layanan aplikasi digunakan untuk mengontrol daemon sistem dan negara mereka berjalan dan memberikan gambaran yang berguna. Anda juga dapat mengontrol layanan yang start up pada saat boot.';
+$lang['services_app_name'] = 'Jasa';
+$lang['services_boot_status'] = 'Memulai';
+$lang['services_description'] = 'Deskripsi layanan';
+$lang['services_service'] = 'Layanan';
+$lang['services_services'] = 'Jasa';
