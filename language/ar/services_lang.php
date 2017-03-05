fa161a8b923fd17faf2b15d9d92539e8d3d4f5fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:55:57 +0000

    Add new files and directories and commit changes to local repository

--- language/ar/services_lang.php
+++ language/ar/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'يتم استخدام التطبيق خدمات للسيطرة على الشياطين النظام والدولة تشغيل ويقدم نظرة مفيدة. ';
+$lang['services_app_description'] = 'يتم استخدام التطبيق خدمات للسيطرة على الشياطين النظام والدولة تشغيل ويقدم نظرة مفيدة. يمكنك أيضا التحكم التي تبدأ الخدمات حتى في وقت التمهيد.';
 $lang['services_app_name'] = 'خدمات';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_boot_status'] = 'أبدء';
+$lang['services_description'] = 'وصف الخدمة';
+$lang['services_service'] = 'الخدمات';
+$lang['services_services'] = 'خدمات';
