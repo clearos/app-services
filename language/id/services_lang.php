91186a2640635f7caba01db2561e4fa2d1ede35a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:53:32 +0000

    Add new files and directories, commit changes, push changes

--- language/id/services_lang.php
+++ language/id/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Layanan aplikasi digunakan untuk mengontrol daemon sistem dan negara mereka berjalan dan memberikan gambaran yang berguna. Anda juga dapat mengontrol layanan yang start up pada saat boot.';
+$lang['services_app_description'] = 'Aplikasi Layanan digunakan untuk mengendalikan daemon sistem dan status pengoperasiannya dan memberikan gambaran yang berguna. Anda juga dapat mengontrol layanan mana yang akan dimulai saat boot.';
 $lang['services_app_name'] = 'Jasa';
 $lang['services_boot_status'] = 'Memulai';
-$lang['services_description'] = 'Deskripsi layanan';
+$lang['services_description'] = 'Deskripsi Layanan';
 $lang['services_service'] = 'Layanan';
 $lang['services_services'] = 'Jasa';
