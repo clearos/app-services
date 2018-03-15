012d442acfe982051366522e9c4448f67058f66a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:17:08 +0000

    Add new files and directories, commit changes, push changes

--- language/so/services_lang.php
+++ language/so/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'app Adeegyada waxaa loo isticmaalaa si loo xakameeyo daemons nidaamka gobolka iyo orodka iyo bixinaya aragti guud oo ah mid waxtar leh. Waxa kale oo aad xakamayn kartid adeegyada bilowdo waqtiga boot.';
+$lang['services_app_description'] = 'Barnaamijka Adeega waxaa loo isticmaalaa in lagu xakameeyo qashinka nidaamka iyo xaaladda ay ku sugan yihiin waxayna bixiyaan muuqaal guud oo waxtar leh. Waxaad sidoo kale xakamayn kartaa adeegyada ay ka bilaabanayaan wakhtiga wada-shaqeynta.';
 $lang['services_app_name'] = 'Adeegyada';
-$lang['services_boot_status'] = 'xawaaladaha';
-$lang['services_description'] = 'Adeegga Description';
-$lang['services_service'] = 'Service';
+$lang['services_boot_status'] = 'Bilowga';
+$lang['services_description'] = 'Sharaxaadda Adeegga';
+$lang['services_service'] = 'Adeeg';
 $lang['services_services'] = 'Adeegyada';
