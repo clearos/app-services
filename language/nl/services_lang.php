af24342710292366192d16c3fd9462ba7d5f4002
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:09:17 +0000

    Add new files and directories, commit changes, push changes

--- language/nl/services_lang.php
+++ language/nl/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'De Services app wordt gebruikt om het systeem daemons en hun systeem van toezicht op en geeft een nuttig overzicht. U kunt ook bepalen welke diensten starten bij het opstarten.';
-$lang['services_app_name'] = 'diensten';
+$lang['services_app_description'] = 'De Services-app wordt gebruikt om de systeemdaemons en hun actieve status te beheren en biedt een nuttig overzicht. U kunt ook bepalen welke services opstart tijdens het opstarten.';
+$lang['services_app_name'] = 'Diensten';
 $lang['services_boot_status'] = 'Beginnen';
-$lang['services_description'] = 'dienst Omschrijving';
+$lang['services_description'] = 'Servicebeschrijving';
 $lang['services_service'] = 'Service';
-$lang['services_services'] = 'diensten';
+$lang['services_services'] = 'Diensten';
