9f0dd3be1d11bf82328e2961d578f08b2af81d40
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:45:49 +0000

    Add new files and directories, commit changes, push changes

--- language/da/services_lang.php
+++ language/da/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Servicedirektivet app bruges til at styre systemet dæmoner og deres kørende tilstand og giver et nyttigt overblik. Du kan også styre hvilke tjenester starter op ved opstart.';
+$lang['services_app_description'] = 'Appen Services bruges til at styre systemdemoner og deres løbende tilstand og giver et nyttigt overblik. Du kan også kontrollere hvilke tjenester der starter ved opstartstidspunktet.';
 $lang['services_app_name'] = 'Services';
 $lang['services_boot_status'] = 'Start op';
-$lang['services_description'] = 'servicebeskrivelse';
+$lang['services_description'] = 'Servicebeskrivelse';
 $lang['services_service'] = 'Service';
 $lang['services_services'] = 'Services';
