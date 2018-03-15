44485750f10e7e5afcaf072ee5935af8d53c4811
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:38:03 +0000

    Add new files and directories, commit changes, push changes

--- language/hy/services_lang.php
+++ language/hy/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'The Ծառայություններ ծրագիրը օգտագործվում է վերահսկել համակարգի daemons եւ դրանց վարման պետություն եւ ապահովում է օգտակար ակնարկ: Դուք նաեւ կարող եք վերահսկել, որը ծառայություններ է սկսնակ է boot ժամանակ.';
+$lang['services_app_description'] = 'Ծառայությունների հավելվածը օգտագործվում է վերահսկելու համակարգի դինամոններն ու դրանց վիճակը եւ ապահովում է օգտակար ակնարկ: Դուք նաեւ կարող եք վերահսկել, թե որ ծառայությունները կսկսվեն boot time- ում:';
 $lang['services_app_name'] = 'Ծառայություններ';
-$lang['services_boot_status'] = 'Նորաստեղծ';
-$lang['services_description'] = 'Service նկարագրություն';
-$lang['services_service'] = 'ծառայություն';
+$lang['services_boot_status'] = 'Սկսնակ';
+$lang['services_description'] = 'Ծառայության նկարագրությունը';
+$lang['services_service'] = 'Ծառայություն';
 $lang['services_services'] = 'Ծառայություններ';
