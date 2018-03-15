6d3306a478824febcd61674c8bd2ecb62e73bb23
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:46:49 +0000

    Add new files and directories, commit changes, push changes

--- language/et/services_lang.php
+++ language/et/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Teenused app kasutatakse kontrolli süsteemi deemoneid ja nende jooksvad riigi ja annab kasuliku ülevaate. Samuti saab kontrollida, millised teenused käivitu alglaadimise ajal.';
-$lang['services_app_name'] = 'teenused';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_app_description'] = 'Rakendust Teenused kasutatakse süsteemi daimonide ja nende jooksva oleku juhtimiseks ning annab kasulikku ülevaadet. Samuti saate juhtida, millised teenused käivitavad käivitamise ajal.';
+$lang['services_app_name'] = 'Teenused';
+$lang['services_boot_status'] = 'Käivitamine';
 $lang['services_description'] = 'Teenuse kirjeldus';
-$lang['services_service'] = 'teenus';
-$lang['services_services'] = 'teenused';
+$lang['services_service'] = 'Teenindus';
+$lang['services_services'] = 'Teenused';
