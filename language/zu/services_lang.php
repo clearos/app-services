7bcea3ffdf67bda3b56dd54832f700fe38db9e1e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:27:39 +0000

    Add new files and directories, commit changes, push changes

--- language/zu/services_lang.php
+++ language/zu/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Uhlelo lokusebenza Services isetshenziswa ukulawula daemons uhlelo kanye nesimo sabo running futhi inikeza umbono ewusizo. Ungakwazi futhi ukulawula yiziphi izinsizakalo qalisa qalisa sikhathi.';
-$lang['services_app_name'] = 'Imisebenzi';
+$lang['services_app_description'] = 'Uhlelo lokusebenza lwezinsizakalo lisetshenziselwa ukulawula amademoni wesistimu nokusebenza kwawo futhi inikeza ukubuka konke okuwusizo. Ungakwazi futhi ukulawula ukuthi yimaphi amasevisi aqala ngesikhathi sokuqalisa.';
+$lang['services_app_name'] = 'Izinsizakalo';
 $lang['services_boot_status'] = 'Qalisa';
-$lang['services_description'] = 'Izikhungo incazelo';
-$lang['services_service'] = 'Izikhungo';
-$lang['services_services'] = 'Imisebenzi';
+$lang['services_description'] = 'Incazelo yesevisi';
+$lang['services_service'] = 'Isevisi';
+$lang['services_services'] = 'Izinsizakalo';
