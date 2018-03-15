3ef78bf503b39d263ef04a44f214dbbe86653e8e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:59:41 +0000

    Add new files and directories, commit changes, push changes

--- language/ko/services_lang.php
+++ language/ko/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = '서비스 응용 프로그램은 시스템 데몬 및 실행 상태를 제어하는 ​​데 사용하고 유용한 개요를 제공합니다. 또한 서비스가 부팅시에 시작되는 제어 할 수 있습니다.';
+$lang['services_app_description'] = '서비스 응용 프로그램은 시스템 데몬과 실행 상태를 제어하는 ​​데 사용되며 유용한 개요를 제공합니다. 또한 부팅 할 때 시작할 서비스를 제어 할 수 있습니다.';
 $lang['services_app_name'] = '서비스';
 $lang['services_boot_status'] = '시작';
 $lang['services_description'] = '서비스 설명';
