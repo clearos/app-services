58e6c6c2bbc819c45dd83048e45da4972b3b9d1a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:26:15 +0000

    Add new files and directories, commit changes, push changes

--- language/vi/services_lang.php
+++ language/vi/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Các ứng dụng của dịch vụ được sử dụng để kiểm soát trình nền hệ thống và trạng thái hoạt động của họ và cung cấp một cái nhìn tổng quan hữu ích. Bạn cũng có thể kiểm soát các dịch vụ khởi động lúc khởi động.';
+$lang['services_app_description'] = 'Ứng dụng Dịch vụ được sử dụng để kiểm soát các trình nền hệ thống và trạng thái hoạt động của chúng và cung cấp tổng quan hữu ích. Bạn cũng có thể kiểm soát dịch vụ nào khởi động khi khởi động.';
 $lang['services_app_name'] = 'Dịch vụ';
-$lang['services_boot_status'] = 'Startup';
+$lang['services_boot_status'] = 'Khởi động';
 $lang['services_description'] = 'Dịch vụ Mô tả';
 $lang['services_service'] = 'Dịch vụ';
 $lang['services_services'] = 'Dịch vụ';
