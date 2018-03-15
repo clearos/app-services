12bb6cefb7935e9ceade5180f637d297f0f5b7f8
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:53:08 +0000

    Add new files and directories, commit changes, push changes

--- language/hu/services_lang.php
+++ language/hu/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = 'A Services alkalmazás ellenőrzésére használják a rendszert démonok és működési állapotát, és egy hasznos áttekintést. Azt is szabályozhatja, hogy mely szolgáltatások indulnak el a rendszer indításakor.';
+$lang['services_app_description'] = 'A Szolgáltatások alkalmazás a rendszerdémonok és azok futási állapotának ellenőrzésére szolgál, és hasznos áttekintést nyújt. Azt is ellenőrizheti, hogy mely szolgáltatások indulnak el a rendszerindításkor.';
 $lang['services_app_name'] = 'Szolgáltatások';
 $lang['services_boot_status'] = 'üzembe helyezés';
 $lang['services_description'] = 'szolgáltatás leírása';
