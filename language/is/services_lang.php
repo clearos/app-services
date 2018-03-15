a290fd28532d634676deacae74d055b6af7f53f9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:55:35 +0000

    Add new files and directories, commit changes, push changes

--- language/is/services_lang.php
+++ language/is/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'The Services app er notað til að stjórna kerfinu daemons og gangi ástand þeirra og veitir gagnlegar yfirlit. Þú getur einnig stjórnað hvaða þjónustu byrjar upp við ræsingu.';
+$lang['services_app_description'] = 'Þjónusta-appið er notað til að stjórna kerfinu, djöfulnum og hlaupandi ástandinu og gefur gagnlegt yfirlit. Þú getur einnig stjórnað hvaða þjónustu byrjar í ræsingu.';
 $lang['services_app_name'] = 'Þjónusta';
-$lang['services_boot_status'] = 'gangsetning';
-$lang['services_description'] = 'þjónusta Lýsing';
-$lang['services_service'] = 'þjónusta';
+$lang['services_boot_status'] = 'Uppsetning';
+$lang['services_description'] = 'Þjónusta Lýsing';
+$lang['services_service'] = 'Þjónusta';
 $lang['services_services'] = 'Þjónusta';
