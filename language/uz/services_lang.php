beb7afb557de18f81bedbe3a26a2930bf51ef5a5
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:25:30 +0000

    Add new files and directories, commit changes, push changes

--- language/uz/services_lang.php
+++ language/uz/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Xizmatlar Ilova tizim Daimons va ularning yugurish holatini nazorat qilish uchun foydalaniladigan va foydali umumiy beradi. Bundan tashqari, xizmatlar boot vaqtda boshlash, qaysi nazorat qilish mumkin.';
-$lang['services_app_name'] = 'xizmatlar';
+$lang['services_app_description'] = '"Xizmatlar" ilovasi sistemani boshqarish va ularning ish holatini boshqarish uchun ishlatiladi va foydali ma&#39;lumot beradi. Qaysi xizmatlarning yuklash paytida boshlanishini nazorat qilishingiz mumkin.';
+$lang['services_app_name'] = 'Xizmatlar';
 $lang['services_boot_status'] = 'Ish boshlash';
-$lang['services_description'] = 'xizmat Tavsif';
-$lang['services_service'] = 'xizmat';
-$lang['services_services'] = 'xizmatlar';
+$lang['services_description'] = 'Xizmat ta&#39;rifi';
+$lang['services_service'] = 'Xizmat';
+$lang['services_services'] = 'Xizmatlar';
