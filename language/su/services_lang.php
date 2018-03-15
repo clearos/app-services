b55a2963de7224ad4fdd3578c8e07a16c5558f68
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:18:53 +0000

    Add new files and directories, commit changes, push changes

--- language/su/services_lang.php
+++ language/su/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Nu Services app dipaké pikeun ngadalikeun daemons sistim jeung kaayaan jalan maranéhanana jeung nyadiakeun Tinjauan dipake. Anjeun oge bisa ngadalikeun ladenan nu ngamimitian nepi dina waktu boot.';
-$lang['services_app_name'] = 'Services';
+$lang['services_app_description'] = 'The Layanan aplikasi anu dipaké pikeun ngadalikeun daemons sistem na kaayaan jalan maranéhanana sarta nyadiakeun Tinjauan mangpaat. Anjeun oge bisa ngadalikeun jasa nu ngamimitian nepi wanoh boot.';
+$lang['services_app_name'] = 'jasa';
 $lang['services_boot_status'] = 'Ngamimitian';
-$lang['services_description'] = 'Service Description';
+$lang['services_description'] = 'Service Pedaran';
 $lang['services_service'] = 'palayanan';
-$lang['services_services'] = 'Services';
+$lang['services_services'] = 'jasa';
