678e8ab0d4dc7d63c7d9de9be1107b84346d05da
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:44:58 +0000

    Add new files and directories, commit changes, push changes

--- language/cs/services_lang.php
+++ language/cs/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Aplikace Services se používá k ovládání systému démony a jejich zapnutého stavu a poskytuje užitečný přehled. Můžete také určit, které služby spuštění při startu systému.';
-$lang['services_app_name'] = 'služby';
+$lang['services_app_description'] = 'Aplikace Služby slouží k ovládání systémových démonů a jejich běhu a poskytuje užitečný přehled. Můžete také řídit, které služby se spouštějí při spuštění počítače.';
+$lang['services_app_name'] = 'Služby';
 $lang['services_boot_status'] = 'Uvedení do provozu';
 $lang['services_description'] = 'Popis služby';
 $lang['services_service'] = 'Servis';
-$lang['services_services'] = 'služby';
+$lang['services_services'] = 'Služby';
