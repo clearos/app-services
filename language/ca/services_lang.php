d65ef39425ae018b10630e521a7ffc76cf344a8a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:42:27 +0000

    Add new files and directories, commit changes, push changes

--- language/ca/services_lang.php
+++ language/ca/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'L&#39;aplicació Serveis s&#39;utilitza per controlar els dimonis del sistema i el seu estat d&#39;execució i proporciona un panorama útil. També pot controlar quins serveis s&#39;inicien en temps d&#39;arrencada.';
+$lang['services_app_description'] = 'L&#39;aplicació Serveis s&#39;utilitza per controlar els dimonis del sistema i el seu estat en execució i proporciona una visió general útil. També podeu controlar quins serveis s&#39;inicien a l&#39;hora d&#39;arrencada.';
 $lang['services_app_name'] = 'Serveis';
-$lang['services_boot_status'] = 'd&#39;inici';
+$lang['services_boot_status'] = 'Inici';
 $lang['services_description'] = 'Descripció del servei';
-$lang['services_service'] = 'servei';
+$lang['services_service'] = 'Servei';
 $lang['services_services'] = 'Serveis';
