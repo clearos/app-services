d9f463c6297c5fc8312cf9039787178f91c6d5cb
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:56:49 +0000

    Add new files and directories, commit changes, push changes

--- language/ja/services_lang.php
+++ language/ja/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'サービスアプリケーションは、システムデーモンとその実行状態を制御するために使用される有用な概要を提供します。 また、サービスがブート時に起動するかを制御することができます。';
+$lang['services_app_description'] = 'サービスアプリケーションは、システムデーモンとその実行状態を制御するために使用され、有用な概要を提供します。 起動時に起動するサービスを制御することもできます。';
 $lang['services_app_name'] = 'サービス';
 $lang['services_boot_status'] = '起動';
-$lang['services_description'] = 'サービス記述';
+$lang['services_description'] = 'サービスの説明';
 $lang['services_service'] = 'サービス';
 $lang['services_services'] = 'サービス';
