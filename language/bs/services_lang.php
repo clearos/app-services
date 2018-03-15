0dbcf5e43eb44860b7b732ca744c94faba98c6f9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:40:50 +0000

    Add new files and directories, commit changes, push changes

--- language/bs/services_lang.php
+++ language/bs/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Usluge aplikacija se koristi za kontrolu sistema demone i njihov rad državnih i daje koristan pregled. Također se možete kontrolirati koje usluge pokreće u vrijeme pokretanja.';
-$lang['services_app_name'] = 'usluge';
+$lang['services_app_description'] = 'Aplikacija Uslugi se koristi za kontrolu nad sistemskim demonomima i njihovom funkcijom i pruža korisni pregled. Takođe možete kontrolisati koje usluge pokreću prilikom pokretanja.';
+$lang['services_app_name'] = 'Usluge';
 $lang['services_boot_status'] = 'Startup';
 $lang['services_description'] = 'Opis usluge';
-$lang['services_service'] = 'usluga';
-$lang['services_services'] = 'usluge';
+$lang['services_service'] = 'Servis';
+$lang['services_services'] = 'Usluge';
