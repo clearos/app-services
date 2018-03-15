a19a0daf9c4bfd9b409f23ee1188fcb66b3af389
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:10:06 +0000

    Add new files and directories, commit changes, push changes

--- language/no/services_lang.php
+++ language/no/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = 'Tjenestedirektivet app brukes til å kontrollere systemet nisser og deres kjører tilstand og gir en nyttig oversikt. Du kan også styre hvilke tjenester starter opp ved oppstart.';
+$lang['services_app_description'] = 'Tjenesteprogrammet brukes til å kontrollere systemdemoner og deres løpestatus og gir en nyttig oversikt. Du kan også kontrollere hvilke tjenester som starter opp ved oppstartstidspunktet.';
 $lang['services_app_name'] = 'tjenester';
 $lang['services_boot_status'] = 'oppstart';
 $lang['services_description'] = 'Tjenestebeskrivelse';
