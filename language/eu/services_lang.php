f8a73b7db4097e5bb544f362c9cf214de232552b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:38:49 +0000

    Add new files and directories, commit changes, push changes

--- language/eu/services_lang.php
+++ language/eu/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Zerbitzuak aplikazioa sistema daemon eta beraien entzierro egoera kontrolatzeko erabiltzen da, eta ikuspegi erabilgarria eskaintzen du. Ere kontrola dezakezu zein zerbitzuak martxan abioan.';
+$lang['services_app_description'] = 'Zerbitzuen aplikazioa sistemaren daemons kontrolatzeko eta bere exekuzio egoera kontrolatzeko erabiltzen da eta ikuspegi baliagarria eskaintzen du. Gainera, abioan abiarazten diren zerbitzuak kontrolatzen dituzu.';
 $lang['services_app_name'] = 'Zerbitzuak';
 $lang['services_boot_status'] = 'Hasi';
-$lang['services_description'] = 'zerbitzu deskribapena';
+$lang['services_description'] = 'Zerbitzuen deskribapena';
 $lang['services_service'] = 'zerbitzua';
 $lang['services_services'] = 'Zerbitzuak';
