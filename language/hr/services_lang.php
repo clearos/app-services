6e5d48986e3da0285c6997a8b21b7299b7991c47
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:44:35 +0000

    Add new files and directories, commit changes, push changes

--- language/hr/services_lang.php
+++ language/hr/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = 'Njezin je aplikacija se koristi za kontrolu demone sustava i njihovo trčanje stanje i daje koristan pregled. Također možete kontrolirati koje usluge pokreće prilikom podizanja sustava.';
+$lang['services_app_description'] = 'Aplikacija Usluge koristi se za kontrolu demona sustava i njihovog stanja, te pruža korisni pregled. Također možete kontrolirati koje se usluge pokreću pri dizanju.';
 $lang['services_app_name'] = 'usluge';
 $lang['services_boot_status'] = 'Stavljanje u pogon';
 $lang['services_description'] = 'Opis usluge';
