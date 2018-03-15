1a1f5a975ffe2a4018cb6f03914f61c01e4b6f0e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:23:00 +0000

    Add new files and directories, commit changes, push changes

--- language/tl/services_lang.php
+++ language/tl/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Ang Mga Serbisyo app ay ginagamit upang makontrol ang sistema daemons at ang kanilang pagtakbo ng estado at nagbibigay ng isang kapaki-pakinabang na pangkalahatang-ideya. Maaari mo ring kontrolin kung aling mga serbisyo simulan up sa boot oras.';
-$lang['services_app_name'] = 'Services';
+$lang['services_app_description'] = 'Ang Mga Serbisyo app ay ginagamit upang kontrolin ang mga daemon ng system at ang kanilang pagpapatakbo ng estado at nagbibigay ng isang kapaki-pakinabang na pangkalahatang-ideya. Maaari mo ring kontrolin kung aling mga serbisyo ang magsisimula sa oras ng boot.';
+$lang['services_app_name'] = 'Mga Serbisyo';
 $lang['services_boot_status'] = 'Magsimula';
-$lang['services_description'] = 'Serbisyo Paglalarawan';
-$lang['services_service'] = 'serbisyo';
-$lang['services_services'] = 'Services';
+$lang['services_description'] = 'Paglalarawan ng Serbisyo';
+$lang['services_service'] = 'Serbisyo';
+$lang['services_services'] = 'Mga Serbisyo';
