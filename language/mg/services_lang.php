0da542af9fbf80d269e2d8a62b108840a51a9905
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:03:39 +0000

    Add new files and directories, commit changes, push changes

--- language/mg/services_lang.php
+++ language/mg/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = 'Ny Services fampiharana no ampiasaina mba hifehy ny rafitra nihazakazaka daemons sy ny fanjakana, ary manome ny mahasoa topy maso. Azonao atao ihany koa ny mifehy izay asa manomboka amin&#39;ny baotin&#39;i ny fotoana.';
+$lang['services_app_description'] = 'Ny fampiharana Services dia ampiasaina mba hifehezana ny daemon sy ny rafitra fitantanana azy ary manome tombony mahasoa. Azonao atao ihany koa ny manara-maso ny serivisy manomboka amin&#39;ny fotoana boot.';
 $lang['services_app_name'] = 'Services';
 $lang['services_boot_status'] = 'Fiatombohana';
 $lang['services_description'] = 'Service Description';
