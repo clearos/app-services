e9ce5a066da8cb03f5a6e73a50070ceab51c17bd
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:51:05 +0000

    Add new files and directories, commit changes, push changes

--- language/ht/services_lang.php
+++ language/ht/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Se app a Sèvis itilize kontwole damon yo sistèm ak eta kouri yo epi li bay yon BECA itil. Ou kapab tou kontwole ki sèvis kòmanse moute nan moman bòt.';
-$lang['services_app_name'] = 'sèvis';
-$lang['services_boot_status'] = 'startup';
-$lang['services_description'] = 'sèvis Deskripsyon';
-$lang['services_service'] = 'Lapòs';
-$lang['services_services'] = 'sèvis';
+$lang['services_app_description'] = 'App a sèvis yo itilize pou kontwole dyemons yo sistèm ak eta kouri yo epi li bay yon BECA itil. Ou kapab tou kontwole ki sèvis kòmanse nan tan bòt.';
+$lang['services_app_name'] = 'Sèvis';
+$lang['services_boot_status'] = 'Startup';
+$lang['services_description'] = 'Sèvis Deskripsyon';
+$lang['services_service'] = 'Sèvis';
+$lang['services_services'] = 'Sèvis';
