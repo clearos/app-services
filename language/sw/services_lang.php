6d85dd97a9ed68a0428d721176a858d2b2c282f8
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:19:29 +0000

    Add new files and directories, commit changes, push changes

--- language/sw/services_lang.php
+++ language/sw/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Services programu hutumika kudhibiti daemons mfumo na hali zao mbio na inatoa maelezo muhimu. Unaweza pia kudhibiti ambayo huduma kuanza hadi saa Boot wakati.';
-$lang['services_app_name'] = 'Services';
+$lang['services_app_description'] = 'Programu ya Huduma hutumiwa kudhibiti daemoni za mfumo na hali yao ya kuendesha na hutoa maelezo muhimu. Unaweza pia kudhibiti huduma ambazo zinaanza wakati wa boot.';
+$lang['services_app_name'] = 'Huduma';
 $lang['services_boot_status'] = 'Anzisha';
-$lang['services_description'] = 'huduma Maelezo ya';
-$lang['services_service'] = 'huduma';
-$lang['services_services'] = 'Services';
+$lang['services_description'] = 'Maelezo ya Huduma';
+$lang['services_service'] = 'Huduma';
+$lang['services_services'] = 'Huduma';
