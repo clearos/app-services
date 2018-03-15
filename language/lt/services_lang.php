dd661ee25f6ce8cba3667fe16970f69be1340838
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:01:32 +0000

    Add new files and directories, commit changes, push changes

--- language/lt/services_lang.php
+++ language/lt/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Paslaugų programa yra naudojami siekiant kontroliuoti sistemos demonus ir jų važiavimo būklę ir teikia naudingą apžvalgą. Jūs taip pat gali kontroliuoti, kokias paslaugas pradėti įkrovos metu.';
+$lang['services_app_description'] = 'Programos "Paslaugos" naudojama valdyti sistemos daemonus ir jų veikimo būseną bei pateikiama naudinga apžvalga. Taip pat galite nustatyti, kurios paslaugos paleidžiamos paleisties metu.';
 $lang['services_app_name'] = 'Paslaugos';
 $lang['services_boot_status'] = 'Pradėti';
-$lang['services_description'] = 'Paslaugų aprašymas';
-$lang['services_service'] = 'tarnyba';
+$lang['services_description'] = 'aptarnavimo Aprašymas';
+$lang['services_service'] = 'Paslauga';
 $lang['services_services'] = 'Paslaugos';
