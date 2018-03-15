5ff5f3d980ba771b2489aaf444cca0193e465005
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:10:31 +0000

    Add new files and directories, commit changes, push changes

--- language/pa/services_lang.php
+++ language/pa/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'ਸਰਵਿਸਿਜ਼ ਐਪ ਸਿਸਟਮ ਡੈਮਨ ਅਤੇ ਆਪਣੇ ਚੱਲ ਰਾਜ ਦੇ ਕੰਟਰੋਲ ਕਰਨ ਲਈ ਵਰਤਿਆ ਹੈ ਅਤੇ ਇੱਕ ਲਾਭਦਾਇਕ ਸੰਖੇਪ ਜਾਣਕਾਰੀ ਦਿੰਦਾ ਹੈ ਹੈ. ਤੁਹਾਨੂੰ ਇਹ ਵੀ ਕੰਟਰੋਲ ਕਰ ਸਕਦੇ ਹੋ, ਜੋ ਕਿ ਸੇਵਾ ਬੂਟ ਹੋਣ ਤੇ ਸ਼ੁਰੂ.';
-$lang['services_app_name'] = 'ਸਰਵਿਸਿਜ਼';
+$lang['services_app_description'] = 'ਸਰਵਿਸਿਜ਼ ਐਪ ਸਿਸਟਮ ਡੈਮਨ ਅਤੇ ਉਹਨਾਂ ਦੀ ਚਲ ਰਹੀ ਸਟੇਟ ਨੂੰ ਕੰਟਰੋਲ ਕਰਨ ਲਈ ਵਰਤਿਆ ਜਾਂਦਾ ਹੈ ਅਤੇ ਇੱਕ ਉਪਯੋਗੀ ਜਾਣਕਾਰੀ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ. ਤੁਸੀਂ ਇਹ ਵੀ ਨਿਯੰਤਰਿਤ ਕਰ ਸਕਦੇ ਹੋ ਕਿ ਕਿਹੜੀਆਂ ਸੇਵਾਵਾਂ ਬੂਟ ਸਮੇਂ ਸ਼ੁਰੂ ਹੋ ਸਕਦੀਆਂ ਹਨ';
+$lang['services_app_name'] = 'ਸੇਵਾਵਾਂ';
 $lang['services_boot_status'] = 'ਸ਼ੁਰੂ ਕਰਣਾ';
-$lang['services_description'] = 'ਸੇਵਾ ਵੇਰਵਾ';
+$lang['services_description'] = 'ਸੇਵਾ ਦਾ ਵੇਰਵਾ';
 $lang['services_service'] = 'ਸੇਵਾ';
-$lang['services_services'] = 'ਸਰਵਿਸਿਜ਼';
+$lang['services_services'] = 'ਸੇਵਾਵਾਂ';
