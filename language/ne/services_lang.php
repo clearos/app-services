114f432bf8c4b2a4dc7b281c1c2af9fe65b5efcf
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:08:51 +0000

    Add new files and directories, commit changes, push changes

--- language/ne/services_lang.php
+++ language/ne/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'यो सेवा अनुप्रयोगलाई प्रणाली daemons र आफ्नो चलिरहेको राज्य नियन्त्रण गर्न प्रयोग र उपयोगी सिंहावलोकन प्रदान गरिएको छ। तपाईं पनि जो सेवाहरू बुट समयमा सुरु नियन्त्रण गर्न सक्नुहुन्छ।';
-$lang['services_app_name'] = 'सेवा';
-$lang['services_boot_status'] = 'सुरुवातमा';
+$lang['services_app_description'] = 'प्रणाली डेमोन र उनीहरूको चलिरहेको अवस्था नियन्त्रण गर्न सेवा अनुप्रयोग प्रयोग गरिन्छ र उपयोगी अवलोकन प्रदान गर्दछ। तपाइँ पनि नियन्त्रण गर्न सक्नुहुन्छ कुन सेवाहरू बूट समयमा सुरु हुन्छ।';
+$lang['services_app_name'] = 'सेवाहरू';
+$lang['services_boot_status'] = 'स्टार्टअप';
 $lang['services_description'] = 'सेवा विवरण';
 $lang['services_service'] = 'सेवा';
-$lang['services_services'] = 'सेवा';
+$lang['services_services'] = 'सेवाहरू';
