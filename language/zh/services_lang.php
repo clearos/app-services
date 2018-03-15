9af752ee3dece5a87a362da317ef8b411e232dd1
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:43:45 +0000

    Add new files and directories, commit changes, push changes

--- language/zh/services_lang.php
+++ language/zh/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = '该服务的应用程序用来控制系统守护进程和他们的运行状态，并提供了一个有用的概述。 您还可以控制哪些服务在系统启动时启动。';
+$lang['services_app_description'] = 'Services应用程序用于控制系统守护程序及其运行状态并提供有用的概述。 您还可以控制在启动时启动哪些服务。';
 $lang['services_app_name'] = '服务';
 $lang['services_boot_status'] = '启动';
 $lang['services_description'] = '服务说明';
