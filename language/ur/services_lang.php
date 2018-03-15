771a0f777d7ec645a32d2692d149042647bcf6cc
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:25:09 +0000

    Add new files and directories, commit changes, push changes

--- language/ur/services_lang.php
+++ language/ur/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'سروسز ایپ کو سسٹم شیطان اور ان کے چلانے ریاست کو کنٹرول کرنے کے استعمال کیا اور ایک مفید جائزہ فراہم کی جاتی ہے. تم نے بھی خدمات بوٹ وقت شروع ہے جس کو کنٹرول کر سکتے.';
-$lang['services_app_name'] = 'سروسز';
+$lang['services_app_description'] = 'سروس ڈیم سسٹم نظام ڈیمونز اور ان کے چل رہا ہے ریاست کو کنٹرول کرنے اور مفید جائزہ فراہم کرنے کے لئے استعمال کیا جاتا ہے. آپ یہ بھی کنٹرول کرسکتے ہیں کہ کون سا سروس بوٹ وقت پر شروع ہوسکتی ہے.';
+$lang['services_app_name'] = 'خدمات';
 $lang['services_boot_status'] = 'شروع';
 $lang['services_description'] = 'سروس کی تفصیل';
 $lang['services_service'] = 'سروس';
-$lang['services_services'] = 'سروسز';
+$lang['services_services'] = 'خدمات';
