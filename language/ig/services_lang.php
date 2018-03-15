31e96b085e18660486cc9a2484618d9c2bf7deb4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:54:44 +0000

    Add new files and directories, commit changes, push changes

--- language/ig/services_lang.php
+++ language/ig/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'The Services ngwa na-eji aka ịchịkwa usoro daemons na ha na-agba ọsọ ala ma na-enye a bara uru nnyocha. I nwekwara ike ịchịkwa nke ọrụ na-amalite ruo mgbe buut oge.';
-$lang['services_app_name'] = 'Services';
+$lang['services_app_description'] = 'A na - eji ngwa ngwa ọrụ iji chịkwaa usoro daemons na ọnọdụ ha na - agba ọsọ ma na - enye nkọwa dị mma. Ị nwekwara ike ijikwa ọrụ ndị ịmalite na oge buut.';
+$lang['services_app_name'] = 'Ọrụ';
 $lang['services_boot_status'] = 'Malite';
-$lang['services_description'] = 'Service Description';
-$lang['services_service'] = 'Service';
-$lang['services_services'] = 'Services';
+$lang['services_description'] = 'Nkọwa Ọrụ';
+$lang['services_service'] = 'Ọrụ';
+$lang['services_services'] = 'Ọrụ';
