dac93a4bef04a3f416e2580c648ec23ea9babc06
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:36:51 +0000

    Add new files and directories, commit changes, push changes

--- language/sq/services_lang.php
+++ language/sq/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'App Services është përdorur për të kontrolluar demonët sistemit dhe gjendjen e tyre të rrjedhshëm dhe jep një pasqyrë të dobishme. Ju gjithashtu mund të kontrollojë cilat shërbime të fillojnë deri në kohën e nisjes.';
+$lang['services_app_description'] = 'Aplikacioni i Shërbimeve përdoret për të kontrolluar demonët e sistemit dhe gjendjen e tyre në funksion dhe ofron një pasqyrë të dobishme. Ju gjithashtu mund të kontrolloni se cilat shërbime fillojnë në kohën e nisjes.';
 $lang['services_app_name'] = 'Sherbime';
 $lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'Shërbim Përshkrimi';
+$lang['services_description'] = 'Përshkrimi i Shërbimit';
 $lang['services_service'] = 'shërbim';
 $lang['services_services'] = 'Sherbime';
