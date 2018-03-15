329e2d72a77422b816953d0e4a9e10d5730fea18
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:08:00 +0000

    Add new files and directories, commit changes, push changes

--- language/mt/services_lang.php
+++ language/mt/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Il-app Servizzi huwa użat biex jikkontrolla l-daemons tas-sistema u l-istat tmexxija tagħhom u tipprovdi ħarsa ġenerali siewja. Tista &#39;wkoll jikkontrolla liema servizzi jibdew fil-ħin boot.';
+$lang['services_app_description'] = 'L-applikazzjoni tas-Servizzi tintuża biex tikkontrolla d-denim tas-sistema u l-istat ta &#39;tħaddim tagħhom u tipprovdi ħarsa ġenerali utli. Tista &#39;wkoll tikkontrolla liema servizzi jibdew fil-ħin tal-boot.';
 $lang['services_app_name'] = 'Servizzi';
 $lang['services_boot_status'] = 'Ibda';
-$lang['services_description'] = 'servizz Deskrizzjoni';
-$lang['services_service'] = 'servizz';
+$lang['services_description'] = 'Deskrizzjoni tas-Servizz';
+$lang['services_service'] = 'Servizz';
 $lang['services_services'] = 'Servizzi';
