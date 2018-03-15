031816694d26b08cbfce6ab549714992c6af9524
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:23:53 +0000

    Add new files and directories, commit changes, push changes

--- language/tr/services_lang.php
+++ language/tr/services_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['services_app_description'] = 'Hizmetler Uygulamanın sistem cinleri ve çalışma durumunu kontrol etmek için kullanılan ve yararlı bir bakış sağlar. Ayrıca hizmet boot zamanında başlatmak kontrol edebilirsiniz.';
+$lang['services_app_description'] = 'Hizmetler uygulaması, sistem dosyalarını ve çalışma durumlarını kontrol etmek için kullanılır ve yararlı bir genel bakış sağlar. Ayrıca, hangi servislerin açılışta başlatılacağını da kontrol edebilirsiniz.';
 $lang['services_app_name'] = 'Hizmetler';
 $lang['services_boot_status'] = 'Başlamak';
 $lang['services_description'] = 'Servis açıklaması';
