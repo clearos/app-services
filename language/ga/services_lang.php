b1f839717b56b43618bd4270c93138b3d8d52e26
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:54:20 +0000

    Add new files and directories, commit changes, push changes

--- language/ga/services_lang.php
+++ language/ga/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Is é an app Seirbhísí a úsáidtear chun rialú a dhéanamh ar na ndeamhan chórais agus a staid reatha agus cuireann sé forbhreathnú úsáideach. Is féidir leat a rialú chomh maith a thosú seirbhísí suas ag am tosaithe.';
-$lang['services_app_name'] = 'seirbhísí';
-$lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'seirbhís Tuairisc';
-$lang['services_service'] = 'seirbhís';
-$lang['services_services'] = 'seirbhísí';
+$lang['services_app_description'] = 'Úsáidtear an t-iarratas Seirbhísí chun rialú a dhéanamh ar chórais na daonnaí agus ar a staid reatha agus soláthraíonn siad forbhreathnú úsáideach. Is féidir leat rialú a dhéanamh ar na seirbhísí a thosaíonn ar bun ag am tosaithe.';
+$lang['services_app_name'] = 'Seirbhísí';
+$lang['services_boot_status'] = 'Tosaithe';
+$lang['services_description'] = 'Cur Síos Seirbhíse';
+$lang['services_service'] = 'Seirbhís';
+$lang['services_services'] = 'Seirbhísí';
