886df28e2d610f7ae5d61c5c199c4d543eaa44b0
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 15:00:55 +0000

    Add new files and directories, commit changes, push changes

--- language/lo/services_lang.php
+++ language/lo/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'ການ app ບໍລິການໄດ້ຖືກນໍາໃຊ້ໃນການຄວບຄຸມ daemons ລະບົບແລະລັດເຮັດວຽກຂອງເຂົາເຈົ້າແລະການສະຫນອງສະພາບລວມເປັນປະໂຫຍດ. ນອກນັ້ນທ່ານຍັງສາມາດຄວບຄຸມທີ່ການບໍລິການເລີ່ມຕົ້ນຂຶ້ນໃນເວລາ boot.';
-$lang['services_app_name'] = 'ການບໍລິການ';
+$lang['services_app_description'] = 'ແອັບຯບໍລິການຖືກນໍາໃຊ້ເພື່ອຄວບຄຸມລະບົບ daemons ແລະລັດທີ່ກໍາລັງແລ່ນຂອງພວກເຂົາແລະໃຫ້ສະພາບລວມທີ່ເປັນປະໂຫຍດ. ນອກນັ້ນທ່ານຍັງສາມາດຄວບຄຸມການບໍລິການໃດທີ່ຈະເລີ່ມຕົ້ນໃນເວລາການ Boot.';
+$lang['services_app_name'] = 'ບໍລິການ';
 $lang['services_boot_status'] = 'ເລີ່ມ​ຕົ້ນ';
-$lang['services_description'] = 'ລາຍລະອຽດເກມການບໍລິການ';
-$lang['services_service'] = 'ການບໍລິການ';
-$lang['services_services'] = 'ການບໍລິການ';
+$lang['services_description'] = 'ການບໍລິການລາຍລະອຽດ';
+$lang['services_service'] = 'ບໍລິການ';
+$lang['services_services'] = 'ບໍລິການ';
