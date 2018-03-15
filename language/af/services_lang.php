3f9af3eeeffe4102a2f51858ace70b42763fc466
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 14:36:01 +0000

    Add new files and directories, commit changes, push changes

--- language/af/services_lang.php
+++ language/af/services_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['services_app_description'] = 'Die Dienste inligting is gebruik om die stelsel daemons en hul lopende toestand te beheer en bied &#39;n nuttige oorsig. Jy kan ook bepaal watter dienste begin op die boot tyd.';
+$lang['services_app_description'] = 'Die Dienste-program word gebruik om die stelsel-daemonne en hul hardloopstaat te beheer en bied &#39;n nuttige oorsig. U kan ook beheer oor watter dienste by opstart tyd begin.';
 $lang['services_app_name'] = 'dienste';
 $lang['services_boot_status'] = 'Startup';
-$lang['services_description'] = 'diens beskrywing';
-$lang['services_service'] = 'Service';
+$lang['services_description'] = 'Diensbeskrywing';
+$lang['services_service'] = 'diens';
 $lang['services_services'] = 'dienste';
